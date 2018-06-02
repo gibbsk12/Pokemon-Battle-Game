15470c3c2fe52bff1fac7d305d5e50052b893b56
Kate Gibbs <kategibbs@Kates-MBP-2.fios-router.home>
2018-06-02 08:41:05 -0400

    created character object

---
+++ assets/javascript/game.js
@@ -0,0 +1,35 @@
+var characters = {
+  'ampharos': {
+      name: 'ampharos',
+      healthPoints: 120,
+      attackPower: 8,
+      attackAddend: 8,
+      counterAttackPower: 2,
+      imageUrl: './assets/images/ampharos.png'
+  },
+  'raichu': {
+      name: 'raichu',
+      healthPoints: 100,
+      attackPower: 9,
+      attackAddend: 9,
+      counterAttackPower: 5,
+      imageUrl: './assets/images/raichu.png'
+  },
+  'luxray': {
+      name: 'luxray',
+      healthPoints: 150,
+      attackPower: 6,
+      attackAddend: 6,
+      counterAttackPower: 20,
+      imageUrl: './assets/images/luxray.png'
+  },
+  'jolteon': {
+      name: 'jolteon',
+      healthPoints: 180,
+      attackPower: 4,
+      attackAddend: 4,
+      counterAttackPower: 25,
+      imageUrl: './assets/images/jolteon.png'
+  }
+}
+
